f1e0f22d2a284c827c83ac26b0a44c1fe2d0dccc
oskarrrrvk <90181565+oskarrrrvk@users.noreply.github.com>
2022-07-19 17:02:25 +0200

    Delete JuegosTablero directory

--- JuegosTablero/Ajedrez/JugadorAjedrez.java
+++
@@ -1,4 +0,0 @@
-package JuegosTablero.Ajedrez;
-
-public class JugadorAjedrez {
-}
--- JuegosTablero/Exceptions/MovimientoProhibidoException.java
+++
@@ -1,7 +0,0 @@
-package JuegosTablero.Exceptions;
-
-public class MovimientoProhibidoException extends Exception{
-    public  MovimientoProhibidoException(){
-        super("No se puede hacer esa accion");
-    }
-}
